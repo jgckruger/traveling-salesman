52446272a051bab2d4f2ad35713de2992e245f12
João <jgckruger@gmail.com>
2020-09-11 15:08:06 -0300

    Timing for execution + new examples

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 from classes.graph import Graph
 
 df = pd.read_csv('input.txt')
@@ -18,8 +19,18 @@ def generate_weighted_edge_list(df):
     return weighted_edge_list
 
 g = Graph(generate_weighted_edge_list(df))
+
+startTime = time.time()
 g.greedy_tsp(start)
+endTime = time.time()
+print('Greedy:', endTime-startTime, 'seconds')
+
+
+startTime = time.time()
 g.brute_force_tsp(start)
+endTime = time.time()
+print('Brute force:', endTime-startTime, 'seconds')
+
 g.draw()
 plt.axis('off')
 plt.show()
